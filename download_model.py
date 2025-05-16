d5e0e145ebbbecf6d1084b3becfb65f71b8e6bea
Ak1n02 <akinates@marun.edu.tr>
2025-05-16 20:36:42 +0300

    refactor app.py and download_model.py for improved structure and model downloading

--- download_model.py
+++ download_model.py
@@ -1,10 +1,11 @@
 import gdown
 
-FILE_ID_ONNX = "1RQkUdY20r-VMkhUID_gmiZip6LvZuAco"
-FILE_ID_REGNET = "1HMajjuiZjH4I93Pn9EsijmUBScMSKeLy"
-MODEL_PATH_REGNET = "regnet94valacc.pth"
-MODEL_PATH_ONNX = "u2net_human_seg.onnx"
 
+def download():
+    FILE_ID_ONNX = "1RQkUdY20r-VMkhUID_gmiZip6LvZuAco"
+    FILE_ID_REGNET = "1HMajjuiZjH4I93Pn9EsijmUBScMSKeLy"
+    MODEL_PATH_REGNET = "regnet94valacc.pth"
+    MODEL_PATH_ONNX = "u2net_human_seg.onnx"
 
-gdown.download(f"https://drive.google.com/uc?export=download&id={FILE_ID_REGNET}", MODEL_PATH_REGNET, quiet=False)
-gdown.download(f"https://drive.google.com/uc?export=download&id={FILE_ID_ONNX}", MODEL_PATH_ONNX, quiet=False)
+    gdown.download(f"https://drive.google.com/uc?export=download&id={FILE_ID_REGNET}", MODEL_PATH_REGNET, quiet=False)
+    gdown.download(f"https://drive.google.com/uc?export=download&id={FILE_ID_ONNX}", MODEL_PATH_ONNX, quiet=False)
